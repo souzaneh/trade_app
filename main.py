8a2a34d69d257b0a02b07192f84184158f11fdee
Souzaneh <143062282+souzaneh@users.noreply.github.com>
2025-03-03 12:03:40 +0330

    omit matplotlib

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ Original file is located at
 import tensorflow as tf
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
 from keras.models import Sequential
